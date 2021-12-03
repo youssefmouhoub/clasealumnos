f597a89d3527f7fdfc3400238f2813b05cd268f4
youssef <youssefleon75rgmail.com>
2021-12-03 09:53:36 +0100

    3333.02

--- Alumno.java
+++ Alumno.java
@@ -52,15 +52,19 @@ public class Alumno {
     public String getNombreUsuarioGithub() {
         String nombreUsuarioGithub = nombre + numeroMatricula;
         
+        if (nombre.length() > 3 && numeroMatricula.length() > 4) {
+            nombreUsuarioGithub = nombre + numeroMatricula ;
+        }
+        
+        if (nombre.length() < 3 && numeroMatricula.length() > 4) {
+            nombreUsuarioGithub = nombre + numeroMatricula ;
+        }
+        
+        if (nombre.length() > 3 && numeroMatricula.length() < 4) {
+            nombreUsuarioGithub = nombre + numeroMatricula ;
+        }
+        
         if (nombre.length() < 3 && numeroMatricula.length() < 4) {
-            nombreUsuarioGithub = nombre + numeroMatricula ;
-        }
-        
-        if (numeroMatricula.length() < 4) {
-            nombreUsuarioGithub = nombre + numeroMatricula ;
-        }
-        
-        if (nombre.length() < 3) {
             nombreUsuarioGithub = nombre + numeroMatricula ;
         }
         
